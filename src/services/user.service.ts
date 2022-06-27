e07c4ffb5fc1a30059ac021745816743d3f913c4
sonsurim <surim014@naver.com>
2022-06-27 22:16:18 +0900

    refactor: apply auth instance

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,31 +1,12 @@
-import axios from "axios";
-import cookies from "js-cookie";
+import { auth, BASE_URL } from '../apis/index'
 
 class UserService {
   async me() {
-    const accessToken = cookies.get("accessToken");
-    if (!accessToken) {
-      return;
-    }
-
-    const { data } = await axios.get(
-      process.env.NEXT_PUBLIC_API_HOST + "/users/me",
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-
-    return data;
+    return await auth.get(`${BASE_URL}/users/me`);
   }
 
   async read(id: number) {
-    const { data } = await axios.get(
-      process.env.NEXT_PUBLIC_API_HOST + "/users/" + id
-    );
-
-    return data;
+    return await auth.get(`${BASE_URL}/users/${id}`);
   }
 }
 
