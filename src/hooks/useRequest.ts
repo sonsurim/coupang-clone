a42c63db3efdd9d3443de1d56df0017cac852c66
sonsurim <surim014@naver.com>
2022-06-29 00:39:50 +0900

    feat: add useRequest

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,1 +1,5 @@
-export const useRequest = () => {};
+import { QueryOptions, useQuery, UseQueryResult } from "react-query";
+
+type UseRequest = (key: string, func: any, options: QueryOptions) => UseQueryResult
+
+export const useRequest: UseRequest = (key, func, options) => useQuery(key, func, {...options})
